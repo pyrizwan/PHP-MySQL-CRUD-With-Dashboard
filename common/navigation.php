3f827c024fff5103f11d56fc046a2f3f013da77c
Rizwan Shaikh <42799635+pyrizwan@users.noreply.github.com>
2021-06-08 09:40:04 +0530

    Update navigation.php

--- common/navigation.php
+++ common/navigation.php
@@ -14,24 +14,26 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
         <ul class="nav navbar-nav">
-            <li><a href="#">LOGO</a></li>
-            <li><a href="#">Link</a></li>
+            <li><a href="#"><strong>iPerformance</strong></a></li>
+            <li><a href="index.php">Home</a></li>
     		
         </ul>
         <form class="navbar-form navbar-left" role="search">
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Search">
+              <input type="date" name="sdate" class="form-control" placeholder="Search">
+              <input type="date" name="edate" class="form-control" placeholder="Search">
             </div>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <button type="submit" class="btn btn-default">Filter</button>
         </form>
         <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Link</a></li>
+            <li><a href="index.php">Home</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['username'];?> <span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
+                <li><a href="#">API Integration</a></li>
+				 <li class="divider"></li>
+                <li><a href="profile.php">Profile</a></li>
+               
                 <li class="divider"></li>
                 <li><a href="settings.php">Settings</a></li>
                 <li class="divider"></li>
