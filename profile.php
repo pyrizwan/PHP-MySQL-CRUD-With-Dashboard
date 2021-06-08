a1f1542111103096059c3238e41c262a76599674
Rizwan Shaikh <42799635+pyrizwan@users.noreply.github.com>
2021-06-08 09:41:55 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,51 +2,7 @@
 <?php include('config/config.php') ?>
 <?php
 
-	$username=$_SESSION['username'];  
-  $msg='';
-   if (isset($_POST['update_pass'])) {
-   $old_password=$_POST['old_password'];
-   $new_password=$_POST['new_password'];
-   $new_password2=$_POST['new_password2'];
-  
-   if($new_password==$new_password2)
-   {
-		$query = "SELECT * FROM tbl_affiliates WHERE username='$username' AND password='$old_password'";
-		$results = mysqli_query($db, $query);
-		if (mysqli_num_rows($results) == 1) {
-		
-		//logic for change password
-		
-		mysqli_query($db, "update tbl_affiliates set password='$new_password' where username='$username' AND password='$old_password'");
-		$msg='<div class="alert alert-success alert-dismissable">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
-                <strong>Well done!</strong> You successfully updated your password.
-            </div>';
-			
-		
-		
-			
-		}else {
-			$msg='<div class="alert alert-danger alert-dismissable">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
-                <strong>Oh no!</strong> You have enetered wrong old password.
-            </div>';
-		}
-   }
-else
-{
-	$msg='<div class="alert alert-danger alert-dismissable">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
-                <strong>Incorrect!</strong> New password and confirm new password should have the same value.
-            </div>';
-}	
-		
-   
-   
-   
-   }
-   
-   
+$username=$_SESSION['username'];    
 $query = "SELECT * FROM tbl_affiliates WHERE username='$username'";
 $results = mysqli_query($db, $query);
 $record=$results->fetch_object();
